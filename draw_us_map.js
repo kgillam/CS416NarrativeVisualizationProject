6f8cfe93e64521828015c3afd323e7b65041270e
Kirsten Gillam <kgillam2@illinois.edu>
2022-07-29 18:53:25 -0700

    test color gradient

--- draw_us_map.js
+++ draw_us_map.js
@@ -1,13 +1,26 @@
+function update_map(){
+	var svg = d3.select("svg");
+	
+	color_scale = d3.scaleLinear()
+		.domain([0,100])
+		.range(["white", "blue"])
+	//TODO
+	
+}
+
 function draw_map(){	
 	var svg = d3.select("svg");
-
 	var path = d3.geoPath();
-
 	d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 	  if (error) throw error;
 
 	  console.log(promise);
 	  console.log(us);
+
+	color_scale = d3.scaleLinear()
+		.domain([0,100])
+		.range(["white", "blue"])
+			
 	  
 	  svg.append("g")
 		  .attr("class", "states")
@@ -16,7 +29,8 @@ function draw_map(){
 		.enter().append("path")
 		  .attr("d", path)
 		  .style("fill", function(d){ 
-			if(d.id==states.get('CA')){return "blue"} 
+			return color_scale(values_map.get(d.id))
+			//if(d.id==states.get('CA')){return "blue"} 
 		  })
 		  .on("click", function(d,i){
 			  console.log(d.id + " " + i);
